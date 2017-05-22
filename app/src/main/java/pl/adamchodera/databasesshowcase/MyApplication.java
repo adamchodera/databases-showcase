dc33ce57bf2d416b9404a6c447deed9f00edc0f8
Adam Chodera <adam@chodera.com.pl>
2017-05-22 15:35:55 +0200

    Add empty Application class

---
+++ app/src/main/java/pl/adamchodera/databasesshowcase/MyApplication.java
@@ -0,0 +1,12 @@
+package pl.adamchodera.databasesshowcase;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        
+    }
+}
